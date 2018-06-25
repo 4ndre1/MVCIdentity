250fb5967f73702c6b529023c04cf607fdec4c42
Andre <andremco1992@outlook.com>
2018-06-24 23:23:18 -0300

    Init commit

---
+++ MVCIdentityApp/MVCIdentity.App/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCIdentity.App.MvcApplication" Language="C#" %>
---
+++ MVCIdentityApp/MVCIdentity.Identity/Context/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+
+namespace MVCIdentity.Identity.Context
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>, IDisposable
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
